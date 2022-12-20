bcff6d6be937bbc0dcdd84ccb9b85c4ae8fbde05
Alaeddine <alaeddinefattouch@insat.u-carthage.tn>
2022-12-20 18:44:42 +0100

    fixed csrf problem

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,15 @@ from .views import views
 from .auth import auth
 import pymongo
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'random string123'
+    app.config['SECRET_KEY'] = 'insert a secret key'
 
     client = pymongo.MongoClient('localhost', 27017)
     db = client.user_login_system
 
-    
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     return app
--- app/auth.py
+++ app/auth.py
@@ -2,10 +2,12 @@ from flask import Blueprint , request, jsonify
 from .models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, EmailField, IntegerField
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms.validators import InputRequired, Length
 auth = Blueprint('auth', __name__)
 
 class SignupForm(FlaskForm):
+    class Meta:
+        csrf = False
     card_id = IntegerField(validators=[InputRequired()])
     firstname =  StringField(validators=[InputRequired(), Length(min=2, max=20)])
     lastname =  StringField(validators=[InputRequired(), Length(min=2, max=20)])
@@ -24,7 +26,7 @@ def signup():
         password =  signup_form.password.data
         user  = User(card_id, firstname, lastname, email, password)
         return user.to_json()
-    return "Error"
+    return signup_form.errors
 
 @auth.route('/login')
 def login():
